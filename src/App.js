28a74e21c0b94e0829dba0213000e1680b8f5c8a
Züleyha Kurt <netice2017@gmail.com>
2023-01-30 16:21:19 +0300

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
+import CartPage from './pages/CartPage';
+import ProductsPage from './pages/ProductsPage';
+import ErrorPage from './pages/ErrorPage';
 function App() {
   return (
-   <h1>hello</h1>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/products' element={<ProductsPage />} />
+        <Route path='/error' element={<ErrorPage />} />
+      </Routes>
+    
+    </BrowserRouter>
+   
   );
 }
 
